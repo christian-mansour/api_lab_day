f7efde354bc0ea1dc669361d3d5744d8d1510bb4
Christian Mansour <c.mansour@miami.edu>
2016-06-17 10:25:54 -0500

    create welcome message

---
+++ star_wars.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var prompt = require('prompt');
+
+
+//   * CLI prints welcome message
+
+console.log("Welcome to the ultimate Star Wars experience! We are about to take you to a galaxy far, far away...")
+
+//   * CLI prompts the user for input
+//   * CLI makes a simple call to *X* API using the user input
+//   * CLI outputs results
+
+
+// EXTRA FEATURES:
+// --include the opening star wars theme
